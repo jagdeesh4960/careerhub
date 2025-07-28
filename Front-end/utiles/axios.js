c94734d398df454cd7bfaac5cee92095572a2c62
jagdeeshkushwah4960 <jagdeeshkushwah4960@gmail.com>
2025-07-28 13:16:31 +0530

    ok1

--- Front-end/utiles/axios.js
+++ Front-end/utiles/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json", 
